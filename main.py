2a29f22682df0a94e785ab063f0b8af825770716
redicows <redico8008@gmail.com>
2024-03-06 14:41:30 +0700

    add nama_baru

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = ["AHD MUHAJIR","Yosefina","Meyela Anggrini","Vero Agrisanda"]
+name = ["Mei Indah Safitri","Redico Wasinton Siagian","Ryan Muhammad","Yohana Liata Ivanta","Azahra Nafisa"]
 for i in name:
 	print(i)
 
---
+++ namabaru.py
@@ -0,0 +1,4 @@
+names = ["Mei Indah Safitri", "Redico Wasinton Siagian", "Ryan Muhammad", "Yohana Liata Ivanta", "Azahra Nafisag"]
+
+for name in names:
+    print(name)
